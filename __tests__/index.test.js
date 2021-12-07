f163e6f219c23bb84d8f3e6a745f7d8c071a8754
Kaibl <matvei212@gmail.com>
2021-12-07 23:25:48 +0300

    make step 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,10 +9,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff', () => {
+test('JSON', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
   const result = readFile('result.txt');
 
   expect(genDiff(file1, file2)).toBe(result);
+});
+
+test('YML', () => {
+  const file1Y = getFixturePath('file1.yml');
+  const file2Y = getFixturePath('file2.yml');
+  const result = readFile('result.txt');
+
+  expect(genDiff(file1Y, file2Y)).toBe(result);
 });
