4b92916ad147287e3614a8e7993434714316cb1c
Kaibl <matvei212@gmail.com>
2021-12-16 17:27:48 +0300

    fix gen

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import formater from './formatters/index.js';
 const readFile = (file) => fs.readFileSync(path.resolve(process.cwd(), file.trim()), 'utf-8');
 const extractFormat = (filename) => path.extname(filename).slice(1);
 
-const genDiff = (fileOne, fileTwo, format) => {
+const genDiff = (fileOne, fileTwo, format = 'stylish') => {
   const fileOneFormat = extractFormat(fileOne);
   const fileTwoFormat = extractFormat(fileTwo);
   const fileOneContent = readFile(fileOne);
